1d104f82d61ea2d243dfccf34440ab960c9e731f
Tjoeser <86958571+Tjoeser@users.noreply.github.com>
2022-04-05 13:46:25 +0200

    taxi af

--- overig/calculate_price.php
+++ overig/calculate_price.php
@@ -1,15 +1,10 @@
 <!DOCTYPE html>
-<html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="stylesheet" href="form.css">
+    <title>thijs ze mooi taxi fijltje</title>
 </head>
 <body>
 
-<!-- <form method="POST" action="calculate_price.php">
+<form method="post" action="<?php $_SERVER['PHP_SELF']; ?>">
     <input type="text" name="km" placeholder="Aantal kilometer...">
     <br>
     <br>
@@ -17,103 +12,80 @@
     <br>
     <br>
     <label for="date">Datum dat u vertrekt:</label>
-    <input type="date" name="date">
+    <input type="datetime-local" name="date">
     <br>
     <br>
-    <input class="button" type="submit" value="Submit">
-</form> -->
+    <input class="button" type="submit" name="submit" value="Submit">
+</form>
 
 <?php
-
-$html = "";
-$html .= "<form method=\"post\">";
-$html .= "<label for=\"km\">km:</label>";
-$html .= "<input type=\"text\" id=\"km\" name=\"km\" placeholder=\"Vul het aantal km in...\" value=\"\"><br>";
-$html .= "<br>";
-$html .= "<label for=\"time\">Vertrek tijd:</label>";
-$html .= "<input type=\"time\" id=\"time\" name=\"time\" placeholder=\"Vul de vertrek tijd in...\" value=\"\"><br>";
-
-$html .= "<br>";
-$html .= "<label for=\"date\">date:</label>";
-$html .= "<select id=\"date\" name=\"date\" placeholder=\"Selecteer de dag...\" value=\"\"><br>";
-$html .= "<option value=\"Maandag\">Maandag</option>";
-$html .= "<option value=\"Dinsdag\">Dinsdag</option>";
-$html .= "<option value=\"Woensdag\">Woensdag</option>";
-$html .= "<option value=\"Donderdag\">Donderdag</option>";
-$html .= "<option value=\"Vrijdag\">Vrijdag</option>";
-$html .= "<option value=\"Zaterdag\">Zaterdag</option>";
-$html .= "<option value=\"Zondag\">Zondag</option>";
-$html .= "</select>";
-$html .= "<br>";
-$html .= "<br>";
-$html .= "<input type=\"submit\" name=\"submit\" value=\"Bereken\">";
-$html .= "</form>";
-echo $html;
-
 date_default_timezone_set("Europe/Amsterdam");
-// var_dump($_POST);
-//formulier velden in ontvangst nemen
-if (isset($_REQUEST['submit'])) {
-    // Formvelden ophalen
-    $km = $_REQUEST['km'];
-    $time = $_REQUEST['time'];
-    $date = $_REQUEST['date'];
 
-    // Checken of velden leeg zijn
-    if (empty($km)) {
-        echo 'u heeft nog geen km afstand ingevuld<br>';
-    }
-    if (empty($time)) {
-        echo 'u heeft nog geen tijd ingevuld<br>';
-    }
-    if (empty($date)) {
-        echo 'u heeft nog geen dag ingevuld<br>';
-    } else {
+if (isset($_POST['submit'])){
+    $speedkm = 60;
 
-    if ($date == 'Vrijdag' and $time > '22:00' 
-        or $date == 'Zaterdag' and $time < '08:00' || $time > '18:00' 
-        or $date == 'Zondag' and $time < '08:00' || $time > '18:00' 
-        or $date == 'Maandag' and $time < '07:00') {
+    $kmtarief= 1;
+    $laagtarief = 0.25;
+    $hoogtarief = 0.45;
+    $weekendtarief = 1.15;
 
-        $total25= ($km * '1') + ($km * '0.45');
-        $totalweekend25 = $total25 * '1.25';
-        $totaltotalweekend25 = number_format($totalweekend25, 2, ',', '.');
+    $morning_time = 8;
+    $night_time = 18;
+    $startweekendtijd = 22;
+    $eindweekendtijd = 7;
 
-        echo "Door u gekozen: <br>";
-        echo "km: $km <br>";
-        echo "vertrek tijd: $time<br>";
-        echo "Dag: $date <br>";
-        echo "Subtotaal: €$totaltotalweekend25";
-        } else{
-            if ($time < '08:00' || $time > '18:00') {
+    $eindagweekend = "Maandag";
+    $startdagweekend = "Vrijdag";
 
-                $total45 = ($km * '1') + ($km * '0.45');
-                $total45format = number_format($total45, 2, ',', '.');
+    $total = "";
 
-                echo "Door u gekozen: <br>";
-                echo "km: $km <br>";
-                echo "Vertrek tijd: $time <br>";
-                echo "Dag: $date<br>";
-                echo "Subtotaal: €$total45format";
-            }else{
-                   // Normaal 0.25 tarief
-                   if ($time > '08:00' || $time < '18:00') {
-                       $total25 = ($km * '1') + ($km * '0.25');
-                       $total25format = number_format($total25, 2, ',', '.');
+    $km = $_POST['km'];
+    $time = $_POST['time'];
+    $date = $_POST['date'];
 
-                       echo "Door u gekozen: <br>";
-                       echo "km: $km <br>";
-                       echo "Vertrek tijd: $time <br>";
-                       echo "Dag: $date <br>";
-                       echo "Subtotaal: €$total25format";
-                   }
-            }
-        }
-    }
+    $subtotaalkm = $km * $kmtarief;
+    var_dump($subtotaalkm);
+
+    // $totalminutes = $subtotaalkm 
+
+    if ($time <= $morning_time){
+        $tarief = $hoogtarief;
+    }else{
+        $tarief = $laagtarief;
+    } echo $tarief;
 }
+
+
+
+
+// if (empty($km)) {
+//     echo "km is leeg<br>";
+// } else {
+//     echo "$km km<br>";
+// }
+
+// if(empty($time)){
+//     echo"Tijd is leeg<br>";
+// }else{
+//     echo "$time<br>";
+// }
+// $date = $_POST['date'];
+// if(empty($date)){
+//     echo "Datum is leeg<br>";
+// }else{
+//     echo "$date<br>";
+// }
+
+
+
+echo $time;
+// $totalkm = $km * 1
+
+echo $tarief;
 
 
 ?>
 
 </body>
 </html>
+
